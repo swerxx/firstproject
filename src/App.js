136bf95c13d7539cab5a96f516d1a6a1cd170430
quitLightFE <quitlight101@gmail.com>
2025-09-03 14:32:41 +0500

    loyiha

--- src/App.js
+++ src/App.js
@@ -1,23 +1,125 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="shadow">
+        <div className="container-fluid py-4">
+          <img
+            height="40"
+            src="https://olcha.uz/_nuxt/plus.lRzD4Jf7.png"
+            alt=""
+          />
+        </div>
       </header>
+      <section id="products" className="py-3">
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-sm-6 col-md-4 col-lg-3 productCol">
+              <div className="productCard p-3">
+                <img
+                  className="d-block productImg"
+                  src="https://olcha.uz/image/700x700/products/cdn_1/supplier/stores/1/2025-04-22/joR9rRln6oOypN33UeXRjmPBYSxt3jrWezWqp1C31N19WfG7msfTDbuuNHhC.jpg"
+                  alt=""
+                />
+                <a href="#" className="groupName d-block mb-3 mt-3">
+                  Ноутбуки
+                </a>
+                <p className="price mb-2"> 15 000 000 сум</p>
+                <p className="priceMonth">1 450 000 сум/мес</p>
+                <p className="title">
+                  Асус туф гейминг в/к RTX 3050 память 512/16г
+                </p>
+                <a href="#" className="btn">
+                  Купить
+                </a>
+              </div>
+            </div>
+                <div className="col-sm-6 col-md-4 col-lg-3 productCol">
+              <div className="productCard p-3">
+                <img
+                  className="d-block productImg"
+                  src="https://olcha.uz/image/700x700/products/cdn_1/supplier/stores/1/2025-04-22/joR9rRln6oOypN33UeXRjmPBYSxt3jrWezWqp1C31N19WfG7msfTDbuuNHhC.jpg"
+                  alt=""
+                />
+                <a href="#" className="groupName d-block mb-3 mt-3">
+                  Ноутбуки
+                </a>
+                <p className="price mb-2"> 15 000 000 сум</p>
+                <p className="priceMonth">1 450 000 сум/мес</p>
+                <p className="title">
+                  Асус туф гейминг в/к RTX 3050 память 512/16г
+                </p>
+                <a href="#" className="btn">
+                  Купить
+                </a>
+              </div>
+            </div>
+                <div className="col-sm-6 col-md-4 col-lg-3 productCol">
+              <div className="productCard p-3">
+                <img
+                  className="d-block productImg"
+                  src="https://olcha.uz/image/700x700/products/cdn_1/supplier/stores/1/2025-04-22/joR9rRln6oOypN33UeXRjmPBYSxt3jrWezWqp1C31N19WfG7msfTDbuuNHhC.jpg"
+                  alt=""
+                />
+                <a href="#" className="groupName d-block mb-3 mt-3">
+                  Ноутбуки
+                </a>
+                <p className="price mb-2"> 15 000 000 сум</p>
+                <p className="priceMonth">1 450 000 сум/мес</p>
+                <p className="title">
+                  Асус туф гейминг в/к RTX 3050 память 512/16г
+                </p>
+                <a href="#" className="btn">
+                  Купить
+                </a>
+              </div>
+            </div>
+                <div className="col-sm-6 col-md-4 col-lg-3 productCol">
+              <div className="productCard p-3">
+                <img
+                  className="d-block productImg"
+                  src="https://olcha.uz/image/700x700/products/cdn_1/supplier/stores/1/2025-04-22/joR9rRln6oOypN33UeXRjmPBYSxt3jrWezWqp1C31N19WfG7msfTDbuuNHhC.jpg"
+                  alt=""
+                />
+                <a href="#" className="groupName d-block mb-3 mt-3">
+                  Ноутбуки
+                </a>
+                <p className="price mb-2"> 15 000 000 сум</p>
+                <p className="priceMonth">1 450 000 сум/мес</p>
+                <p className="title">
+                  Асус туф гейминг в/к RTX 3050 память 512/16г
+                </p>
+                <a href="#" className="btn">
+                  Купить
+                </a>
+              </div>
+            </div>
+                <div className="col-sm-6 col-md-4 col-lg-3 productCol">
+              <div className="productCard p-3">
+                <img
+                  className="d-block productImg"
+                  src="https://olcha.uz/image/700x700/products/cdn_1/supplier/stores/1/2025-04-22/joR9rRln6oOypN33UeXRjmPBYSxt3jrWezWqp1C31N19WfG7msfTDbuuNHhC.jpg"
+                  alt=""
+                />
+                <a href="#" className="groupName d-block mb-3 mt-3">
+                  Ноутбуки
+                </a>
+                <p className="price mb-2"> 15 000 000 сум</p>
+                <p className="priceMonth">1 450 000 сум/мес</p>
+                <p className="title">
+                  Асус туф гейминг в/к RTX 3050 память 512/16г
+                </p>
+                <a href="#" className="btn">
+                  Купить
+                </a>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
